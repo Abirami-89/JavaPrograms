5e9f27740bec5ce13ea4d185bc4c83dda4323a10
Krish <abiramideav@gmail.com>
2025-07-03 10:12:07 +0530

    5

---
+++ src/ex_01_java_basic/Lab0002_HelloworldSC.java
@@ -0,0 +1,17 @@
+package ex_01_java_basic;
+
+public class Lab0002_HelloworldSC {
+    //main -public static void- shortcut
+    //Sout
+    public static void main(String[] args) {
+        System.out.println("HelloworldSC");
+    }
+}
+
+
+
+
+
+
+
+

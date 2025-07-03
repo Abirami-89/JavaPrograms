7eca2de65ba1ebe5961663b528484fe956588ab8
Krish <abiramideav@gmail.com>
2025-07-03 10:59:52 +0530

    5

---
+++ src/ex_01_java_basic/Lab003_ShorcutHW.java
@@ -0,0 +1,16 @@
+package ex_01_java_basic;
+
+public class Lab003_ShorcutHW {
+    public static void main(String[] args) {
+        System.out.println("Abirami");
+    }
+
+    public static void main() {
+        //without parameter identifier
+        System.out.println("Abirami");
+    }
+
+
+
+}
+

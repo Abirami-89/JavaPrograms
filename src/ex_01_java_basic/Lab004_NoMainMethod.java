dae4019d35b9757b105f757bf406e616ec931b81
Krish <abiramideav@gmail.com>
2025-07-03 11:09:43 +0530

    5

---
+++ src/ex_01_java_basic/Lab004_NoMainMethod.java
@@ -0,0 +1,6 @@
+package ex_01_java_basic;
+
+public class Lab004_NoMainMethod {
+
+
+}
---
+++ src/ex_01_java_basic/Lab007_Comments_Multi.java
@@ -0,0 +1,13 @@
+package ex_01_java_basic;
+
+public class Lab007_Comments_Multi {
+    public static void main(String[] args) {
+        /*
+         * Author : Pramod Dutta
+         * Course :  ATB10x
+         *
+         */
+        System.out.println("Multi Comment");
+    }
+
+}

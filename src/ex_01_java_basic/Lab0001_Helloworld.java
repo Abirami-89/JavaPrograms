a987c69b2f80785c6e00558dd3c55af10979c620
Krish <abiramideav@gmail.com>
2025-07-03 10:11:54 +0530

    5

--- src/ex_01_java_basic/Lab0001_Helloworld.java
+++ src/ex_01_java_basic/Lab0001_Helloworld.java
@@ -2,7 +2,7 @@ package ex_01_java_basic;
 
 public class Lab0001_Helloworld {
     public static void main  (String[] args) {
-        System .out.println("Hello World!!");
+        System .out.println("Hello World!!E!!!");
     }
 
 
